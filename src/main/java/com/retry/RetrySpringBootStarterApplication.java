df4313ae4a75e5eeb9da673503b4bf17b1d95d16
chenhu <9517530789Hu>
2023-12-21 15:19:40 +0800

    first commit

---
+++ src/main/java/com/retry/RetrySpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrySpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetrySpringBootStarterApplication.class, args);
+    }
+
+}
